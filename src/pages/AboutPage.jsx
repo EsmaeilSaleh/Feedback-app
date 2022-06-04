c30449a1e2797e05d094e7d0334b5d24e632a12d
e.saleh88@gmail.com <e.saleh88@gmail.com>
2022-06-04 16:51:12 +0430

    style about page

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -3,18 +3,28 @@ import { Link } from "react-router-dom";
 
 import Container from "@mui/material/Container";
 import Card from "@mui/material/Card";
+import Button from "@mui/material/Button";
+import HomeIcon from '@mui/icons-material/Home';
+import Typography from '@mui/material/Typography';
 
 const AboutPage = () => {
   return (
-    <Container maxWidth="sm" sx={{  maxWidth: '52em', my: 10  }}>
+    <Container maxWidth="sm" sx={{ maxWidth: "52em", my: 10 }}>
       <h2>AboutPage</h2>
-      <Card sx={{ padding: "1em"}}>
+      <Card sx={{ color: "#989898", background: "#323232", padding: "1em" }}>
         <p>
           Hello, My Name is Esmaeil Saleh. I am a front-end React developer.
-          This is one of my projects which is done using reactjs and mui.
+          This is one of my projects which is done using <span style={{color: 'red'}}>ReactJS</span> and <span style={{ color:'red' }}>MUI</span>.
         </p>
         <p> version: 1.0.0</p>
-        <Link to="/">Back to Home</Link>
+        <Button variant="standard" sx={{display: 'flex',  background: '#000', ml: '50%', transform:'translateX(-50%)'}}>
+          <HomeIcon sx={{color: 'red'}}/>
+          <Typography sx={{ ml: '0.8em', color: 'red' }}>
+          <Link to="/" style={{ color: 'red', textDecoration: "none" }}>
+            Back to Home
+          </Link>
+          </Typography>
+        </Button>
       </Card>
     </Container>
   );
